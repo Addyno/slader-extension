0691101b9f7d5942dc43ea122ed386e727465209
EthanHarv <github@ethanharv.com>
2021-08-02 19:18:25 -0500

    Work on terms instead of specific divs

--- slader-limit-bypass/js/quizlet_bypass.js
+++ slader-limit-bypass/js/quizlet_bypass.js
@@ -1,5 +1,6 @@
-// Remove 'x' free solutions badge.
-if (document.querySelector('.b1if8dab')) { document.querySelector('.b1if8dab').style.display = "none"; }
+// Remove 'x' free solutions badge and "verify your email" badge.
+if (document.querySelector('.BannerWrapper')) { document.querySelector('.BannerWrapper').style.display = "none"; }
+if (document.querySelector('.UINotification')) { document.querySelector('.UINotification').style.display = "none"; }
 
 checkIfNewAccountNeeded();
 
@@ -8,17 +9,17 @@ function checkIfNewAccountNeeded()
     // Three cases: Almost out of solutions, not logged in at all, or out of solutions.
 
     // Almost out of solutions
-    if (document.querySelector('.b1m16m5o') && document.querySelector('.b1m16m5o').textContent == "This is your last free explanation")
+    if (pageContains("This is your last free explanation"))
     {
         signUpNewAccount(false);
     }
     // Not logged in
-    else if (document.querySelector('.t1qexa4p') && document.querySelector('.t1qexa4p').textContent == "Create a free account to see explanations")
+    else if (pageContains("Create a free account to see explanations"))
     {
         signUpNewAccount(true);
     }
     // Out of solutions
-    else if (document.querySelector('.sukvi6d') && document.querySelector('.sukvi6d').textContent == "YOU'VE REACHED YOUR FREE LIMIT")
+    else if (pageContains("YOU'VE REACHED YOUR FREE LIMIT"))
     {
         signUpNewAccount(true);
     }
@@ -27,10 +28,7 @@ function checkIfNewAccountNeeded()
 function signUpNewAccount(doesReload)
 {
     // We're just gonna assume this is a large enough characterspace for it to never matter.
-    var email = "sq_bypass_" + randomString(10, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
-
-    // Same thing ^
-    var username = "sq_bypass_" + randomString(10, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
+    var name = "sq_bypass_" + randomString(10, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
 
     var request = fetch("https://quizlet.com/webapi/3.2/direct-signup", {
         "headers": {
@@ -47,7 +45,7 @@ function signUpNewAccount(doesReload)
         },
         "referrer": "https://quizlet.com/goodbye",
         "referrerPolicy": "origin-when-cross-origin",
-        "body": "{\"TOS\":false,\"birth_day\":\"5\",\"birth_month\":\"5\",\"birth_year\":\"2000\",\"email\":\"" + email + "@example.com\",\"is_free_teacher\":\"2\",\"is_parent\":false,\"password1\":\"SladerBypassPassword\",\"redir\":\"https://quizlet.com/goodbye\",\"signupOrigin\":\"global-header-link\",\"screenName\":\"Logout/logoutMobileSplash\",\"username\":\"" + username + "\",\"marketing_opt_out\":false}",
+        "body": "{\"TOS\":false,\"birth_day\":\"5\",\"birth_month\":\"5\",\"birth_year\":\"2000\",\"email\":\"" + name + "@example.com\",\"is_free_teacher\":\"2\",\"is_parent\":false,\"password1\":\"SladerBypassPassword\",\"redir\":\"https://quizlet.com/goodbye\",\"signupOrigin\":\"global-header-link\",\"screenName\":\"Logout/logoutMobileSplash\",\"username\":\"" + name + "\",\"marketing_opt_out\":false}",
         "method": "POST",
         "mode": "cors",
         "credentials": "include"
@@ -71,3 +69,15 @@ function getToken(){
     token = document.cookie.match("(?:^|;)\\s*" + "qtkn".replace(/[\-\[\]{}()*+?.,\\^$|#\s]/g, "$&") + "=([^;]*)");
     return decodeURIComponent(token[1]);
 };
+
+function pageContains(str)
+{
+    if (document.body.innerHTML.match(str))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
